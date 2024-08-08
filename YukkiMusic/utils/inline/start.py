23b94fb90cdf4e0e95ad099d507ad1520631f96e
Vivanxparth <158141812+Vivanxparth@users.noreply.github.com>
2024-08-08 14:55:43 +0530

    Update start.py
    Signed-off-by: Vivanxparth <158141812+Vivanxparth@users.noreply.github.com>

--- YukkiMusic/utils/inline/start.py
+++ YukkiMusic/utils/inline/start.py
@@ -76,17 +76,10 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
         buttons.append(
             [
                 InlineKeyboardButton(text=_["S_B_7"], user_id=OWNER),
-                InlineKeyboardButton(text=_["S_B_6"], url=f"{GITHUB_REPO}"),
+                
             ]
         )
     else:
-
-        if GITHUB_REPO:
-            buttons.append(
-                [
-                    InlineKeyboardButton(text=_["S_B_6"], url=f""),
-                ]
-            )
 
         if OWNER:
             buttons.append(
@@ -94,7 +87,6 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
                     InlineKeyboardButton(text=_["S_B_7"], user_id=OWNER),
                 ]
             )
-    buttons.append([InlineKeyboardButton(text=_["ST_B_6"], callback_data="")])
     return buttons
 
 
